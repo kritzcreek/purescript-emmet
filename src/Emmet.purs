6aa56cb8f307ecf5446f102d8568fec129f293d1
kRITZCREEK <christoph.hegemann1337@gmail.com>
2017-06-03 06:17:39 +0200

    evalEmmet, parens parsing, html renderer

--- src/Emmet.purs
+++ src/Emmet.purs
@@ -6,13 +6,15 @@ import Control.Alt ((<|>))
 import Control.Lazy (defer)
 import Data.Array as Array
 import Data.Char.Unicode (isDigit)
-import Data.Foldable (class Foldable)
+import Data.Foldable (class Foldable, fold, intercalate)
 import Data.Functor.Nu (Nu)
 import Data.Int as Int
 import Data.List (List, many, some)
 import Data.List as List
-import Data.Maybe (maybe)
-import Data.String (fromCharArray)
+import Data.List.NonEmpty as NE
+import Data.Maybe (Maybe(..), fromMaybe, maybe)
+import Data.String (Pattern(..), fromCharArray, split)
+import Data.Unfoldable (replicate)
 import Matryoshka as M
 import Text.Parsing.Parser (Parser, fail)
 import Text.Parsing.Parser.Combinators as P
@@ -22,6 +24,16 @@ import Text.Parsing.Parser.Token (alphaNum)
 data Attribute
   = Class String
   | Id String
+
+getClass :: Attribute -> Maybe String
+getClass = case _ of
+  Class x -> Just x
+  _ -> Nothing
+
+getId :: Attribute -> Maybe String
+getId = case _ of
+  Id x -> Just x
+  _ -> Nothing
 
 instance showAttribute :: Show Attribute where
   show = case _ of
@@ -34,6 +46,10 @@ data EmmetF a
   | Sibling a a
   | Multiplication a Int
   | Element String (List Attribute)
+
+derive instance functorEmmetF :: Functor EmmetF
+
+type Emmet = Nu EmmetF
 
 element :: String -> List Attribute -> Emmet
 element s as = M.embed (Element s as)
@@ -57,10 +73,6 @@ ppEmmet = M.cata case _ of
   Sibling a b -> "(Sibling " <> a <> " " <> b <> ")"
   Multiplication a n -> "(Multiplication " <> a <> " " <> show n <> ")"
   Element name attrs -> "(Element " <> name <> " " <> show attrs <> ")"
-
-derive instance functorEmmetF :: Functor EmmetF
-
-type Emmet = Nu EmmetF
 
 type EmmetParser a = Parser String a
 
@@ -96,23 +108,138 @@ parseElement = element <$> parseElementName <*> many (parseClass <|> parseId)
 
 parseEmmet :: EmmetParser Emmet
 parseEmmet = do
-  root <- parseElement
+  root <- (defer \_ -> P.between (char '(') (char ')') parseEmmet) <|> parseElement
   P.choice
      [ defer \_ -> parseChild root
      , defer \_ -> parseSibling root
      , defer \_ -> parseParent root
-     , defer \_ -> parseMultiplication root
+     , defer \_ -> do
+          e <- parseMultiplication root
+          P.choice
+            [ defer \_ -> parseChild e
+            , defer \_ -> parseSibling e
+            , defer \_ -> parseParent e
+            , pure e
+            ]
      , pure root
      ]
 
--- to halogen
+evalEmmet :: Emmet -> NE.NonEmptyList HtmlBuilder
+evalEmmet = M.cata case _ of
+  Child os c -> setBuildChildren (NE.toList c) <$> os
+  -- Parent _ _ -> ?x
+  Sibling a b -> a <> b
+  Multiplication a n ->
+    let next = NE.head a
+    in fromMaybe (NE.singleton next) (NE.fromList (replicate n next))
+  Element name attrs ->
+    NE.singleton (htmlBuilder (List.singleton
+      { name, attributes: attributesToHtml attrs, children: List.Nil }))
+  _ -> NE.singleton testDiv
 
-emmetToHalogen :: Emmet -> String
-emmetToHalogen = M.cata case _ of
-  Child _ _ -> ""
-  Parent _ _ -> ""
-  Sibling _ _ -> ""
-  Multiplication _ _ -> ""
-  Element name attrs
-    | List.null attrs -> "HH." <> name <> "_ [ ]"
-    | otherwise -> "HH." <> name <> "[ ] [ ]"
+testDiv :: HtmlBuilder
+testDiv =
+  htmlBuilder
+    (List.singleton
+      {name: "test", attributes: List.Nil, children: List.Nil })
+
+attributesToHtml :: List Attribute -> List HtmlAttribute
+attributesToHtml attrs =
+  let
+    classes = List.mapMaybe getClass attrs
+    ids = List.mapMaybe getId attrs
+    htmlClasses =
+      case List.uncons classes of
+        Nothing -> List.Nil
+        Just {head, tail: List.Nil} ->
+          List.singleton (HtmlClass head)
+        _ ->
+          List.singleton (HtmlClasses classes)
+    htmlId = maybe List.Nil (List.singleton <<< HtmlId) (List.head ids)
+  in
+    htmlId <> htmlClasses
+
+
+data HtmlAttribute
+  = HtmlId String
+  | HtmlClass String
+  | HtmlClasses (List String)
+
+renderHtmlAttribute :: HtmlAttribute -> String
+renderHtmlAttribute = case _ of
+  HtmlId i -> "id=" <> show i
+  HtmlClass c -> "class=" <> show c
+  HtmlClasses cs ->
+    "class=\"" <> List.intercalate " " cs <> "\""
+
+renderHtmlAttributeHalogen :: HtmlAttribute -> String
+renderHtmlAttributeHalogen = case _ of
+  HtmlId i -> "HP.id_ " <> show i
+  HtmlClass c -> "HP.class_ (HH.ClassName " <> show c <> ")"
+  HtmlClasses cs ->
+    "HP.classes [ "
+      <> commaSep (map (\c -> "HH.ClassName " <> show c) cs)
+      <> " ]"
+
+data HtmlBuilderF a = HtmlBuilderF (List (Node a))
+
+derive instance functorHtmlBuilderF :: Functor HtmlBuilderF
+
+type HtmlBuilder = Nu HtmlBuilderF
+
+htmlBuilder :: List (Node HtmlBuilder) -> HtmlBuilder
+htmlBuilder cs = M.embed (HtmlBuilderF cs)
+
+type Node a =
+  { name :: String
+  , attributes :: List HtmlAttribute
+  , children :: List a
+  }
+
+mapNode :: forall a b. (List a -> List b) -> Node a -> Node b
+mapNode f node = node { children = f node.children }
+
+-- node :: String -> List HtmlAttribute -> 
+
+setBuildChildren :: List HtmlBuilder -> HtmlBuilder -> HtmlBuilder
+setBuildChildren cs builder =
+  M.project builder # \(HtmlBuilderF nodes) ->
+   map (mapNode (const cs)) nodes
+   # htmlBuilder
+
+commaSep :: forall f. Foldable f => f String -> String
+commaSep = List.intercalate ", "
+
+renderHtmlBuilder :: HtmlBuilder -> String
+renderHtmlBuilder = M.cata case _ of
+  HtmlBuilderF nodes ->
+    nodes
+      <#> (\ {name, attributes, children} ->
+        "<" <> name <> " "
+        <> List.intercalate " " (map renderHtmlAttribute attributes)
+        <> ">\n"
+        <> List.intercalate "\n" (map (pad 2) children)
+        <> "\n<" <> name <> "/>")
+      # List.intercalate "\n"
+
+pad :: Int -> String -> String
+pad n s = s
+  # split (Pattern "\n")
+  <#> append (fold (Array.replicate n " "))
+  # intercalate "\n"
+
+-- renderHalogen :: Html -> String
+-- renderHalogen = M.cata case _ of
+--   Text t ->
+--     "HH.text " <> show t <> ""
+--   SelfClosing s attrs ->
+--     "HH." <> s <> " [ " <> commaSep (map renderHtmlAttribute attrs) <> " ]"
+--   Node s attrs children
+--     | List.null attrs ->
+--       "HH." <> s
+--       <> "_ [ " <> commaSep children <> " ]"
+--     | otherwise ->
+--       "HH." <> s
+--       <> " [ " <> commaSep (map renderHtmlAttribute attrs)
+--       <> " ] [ " <> commaSep children
+--       <> " ]"
--- src/Main.purs
+++ src/Main.purs
@@ -4,15 +4,19 @@ import Prelude
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log, logShow)
-import Data.Either (Either(..))
+import Data.Either (Either(..), either)
+import Data.Foldable (intercalate)
 import Emmet as Emmet
 import Text.Parsing.Parser (ParseError, runParser)
 
 p :: String -> Either ParseError String
 p s = map Emmet.ppEmmet (runParser s Emmet.parseEmmet)
 
-h :: String -> Either ParseError String
-h s = map Emmet.emmetToHalogen (runParser s Emmet.parseEmmet)
+h :: String -> String
+h s =
+  either show id
+    (map (intercalate "\n" <<< map Emmet.renderHtmlBuilder)
+     (map Emmet.evalEmmet (runParser s Emmet.parseEmmet)))
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
